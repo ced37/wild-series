8ce04b8cd1a62d7aae20afddf28bf61ff8a4a40a
cedric <cedric-perrotin_student2021@wilder.school>
2021-05-21 12:09:02 +0200

    quete 5 routing avancé

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -12,25 +12,38 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
+/**
 
+* @Route("/programs", name="program_")
+
+*/
 class ProgramController extends AbstractController
 
 {
     /**
 
-     * @Route("/programs/", name="program_index")
+      * Correspond à la route /programs/ et au name "program_index"
+
+      * @Route("/", name="index")
+
+      */
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Séries',
+         ]);
+    }
+
+    /**
+     * Correspond à la route /programs/show et au name "program_show"
+
+     * @Route("/{id<\d+>}", methods={"GET"}, name="show")
 
      */
-    public function index(): Response
-
+    public function show(int $id): Response
     {
-
-        return $this->render('program/index.html.twig', [
-
-            'website' => 'Wild Séries',
-     
-         ]);
-
+        return $this->render('program/show.html.twig', ['id' => $id]);
     }
+
 
 }
